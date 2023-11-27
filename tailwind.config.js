8a71e3a1ac7523fc9361e030208b815454639739
alexandru-posirca <151446620+alexandru-posirca@users.noreply.github.com>
2023-11-27 06:08:07 +0200

    Init Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./dist/**/*.html",
+    "./dist/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
