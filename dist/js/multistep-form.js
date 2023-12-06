49c99da1127de3a8c440a3ce891e660abde468b0
alexandru-posirca <axelltech.dev@gmail.com>
2023-12-07 00:00:27 +0200

    Reset data in multiform with close button modal

--- dist/js/multistep-form.js
+++ dist/js/multistep-form.js
@@ -1,3 +1,6 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { auth } from "./main.js";
+
 const steps = document.querySelectorAll(".stp");
 const circleSteps = document.querySelectorAll(".step");
 const nameField = document.getElementById("name");
@@ -14,6 +17,7 @@ const planPrice = document.querySelector(".plan-price");
 const total = document.querySelector(".total b");
 const switcher = document.querySelector(".switch");
 const addons = document.querySelectorAll(".box");
+const closeModalBtn = document.querySelectorAll("[data-close-button]");
 
 let time;
 let currentStep = 1;
@@ -52,6 +56,17 @@ steps.forEach((step) => {
     circleSteps[currentCircle].classList.add("active");
     const stepOne = document.getElementById("step-1");
     stepOne.style.display = "none";
+  });
+});
+
+closeModalBtn.forEach((button) => { 
+  onAuthStateChanged(auth, (user) => {
+    if (user != null) {
+      button.addEventListener("click", () => {
+        currentStep = 1;
+        currentCircle = 0;
+      });
+    }
   });
 });
 
@@ -250,4 +265,8 @@ const setTotal = () => {
     val += Number(res);
   }
   total.innerHTML = `$${val + Number(res)}/${time ? "yr" : "mo"}`;
+};
+
+const setTime = (t) => {
+  return (time = t);
 };
