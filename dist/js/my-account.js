30087fcda64a7b95ea7e659c85204ea36673a1d7
alexandru-posirca <axelltech.dev@gmail.com>
2023-12-08 02:28:06 +0200

    Integration with google auth my account info modal

---
+++ dist/js/my-account.js
@@ -0,0 +1,65 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { auth } from "./main.js";
+
+const openModalBtn = document.querySelectorAll("[data-modal-account]");
+const overlay = document.getElementById("overlay");
+const closeModalBtn = document.querySelectorAll("[data-close-button]");
+const myAccount = document.getElementById("my-account");
+
+openModalBtn.forEach((button) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user != null) {
+      myAccount.setAttribute("data-member", "true");
+      let dataInfoAttr = myAccount.getAttribute("data-member");
+      if (dataInfoAttr === "true") {
+        button.addEventListener("click", () => {
+          const modalJoin = document.querySelector("#my-account");
+          openModalJoin(modalJoin);
+        });
+      }
+    } else {
+      console.log("nelogat");
+      //logic for logout
+    }
+  });
+});
+
+const openModalJoin = (modalJoin) => {
+  if (modalJoin == null) return;
+  let dataInfoAttr = myAccount.getAttribute("data-member");
+  if (dataInfoAttr === "true") {
+    modalJoin.classList.add("active");
+    overlay.classList.add("active");
+  }
+};
+
+const closeModalJoin = (modalJoin) => {
+  if (modalJoin == null) return;
+  modalJoin.classList.remove("active");
+  overlay.classList.remove("active");
+};
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".my-account.active");
+  modals.forEach((modalJoin) => {
+    closeModalJoin(modalJoin);
+  });
+});
+
+closeModalBtn.forEach((button) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user != null) {
+      button.addEventListener("click", () => {
+        console.log("merge close");
+        const modalJoin = document.querySelector("#my-account");
+        closeModalJoin(modalJoin);
+      });
+    } else {
+      console.log("logout succesfull");
+      button.addEventListener("click", () => {
+        const modalJoin = document.querySelector("#modal-join-account");
+        closeModalJoin(modalJoin);
+      });
+    }
+  });
+});
